893d162921671b487358f868ab3c5913a8a7d1c2
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-04-13 23:08:19 -0700

    Finalized solution finding and main function.

--- river_crossing/FWGCTree.py
+++ river_crossing/FWGCTree.py
@@ -1,21 +1,25 @@
 # sheldon woodward
-# DATE
+# 4/13/18
 
 """Tree of all farmer-wolf-goat-cabbage possibilities."""
 
 
-class Tree:
+class FWGCTree:
     states = []
+    solution = None
 
-    def __init__(self, depth=0, state=None):
+    def __init__(self, depth=0, state=None, history=''):
+        self.depth = depth
+        self.history = history
+        self.nodes = []
+        # no state given
         if state is None:
             state = [False, False, False, False]
+        # check for solution
+        if all(state):
+            FWGCTree.solution = history
         # check for loop state
-        Tree.states.append(state)
-        self.depth = depth
-        self.state = state
-        self.nodes = []
-
+        FWGCTree.states.append(state)
         # generate next nodes
         for i in range(4):
             # generate state
@@ -24,13 +28,13 @@ class Tree:
                 new_state[i] = not new_state[i]
             new_state[0] = not new_state[0]
             # add to tree
-            if not Tree.bad_state(new_state):
-                self.nodes.append(Tree(depth + 1, new_state))
+            if not FWGCTree.bad_state(new_state):
+                self.nodes.append(FWGCTree(depth + 1, new_state, history + ('N', 'W', 'G', 'C')[i]))
 
     @staticmethod
     def bad_state(state):
         # pre-existing state
-        if state in Tree.states:
+        if state in FWGCTree.states:
             return True
         # wolf alone with goat
         if state[0] != state[1] and state[1] == state[2]:
--- river_crossing/__init__.py
+++ river_crossing/__init__.py
@@ -1,5 +1,4 @@
 # sheldon woodward
 # 4/13/18
 
-from river_crossing.Tree import *
-from river_crossing.DFS import *
+from river_crossing.FWGCTree import *
