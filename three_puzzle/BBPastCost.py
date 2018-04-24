13e8e6d94c1511a9d5c3be23d0a80c4a4c932c6d
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-04-23 19:08:50 -0700

    Complete problem 3c.

--- three_puzzle/BBPastCost.py
+++ three_puzzle/BBPastCost.py
@@ -19,23 +19,32 @@ class BBPastCost:
         self.nodes = []
 
     def __repr__(self):
-        return str(self.state)
+        display = self.state[:]
+        for i, t in enumerate(display):
+            if t is None:
+                display[i] = 'N'
+        return '\n  ---\n| ' + str(display[0]) + ' ' + str(display[1]) + ' |\n| ' + str(display[2]) + ' ' + str(
+            display[3]) + ' |\n  ---\n'
 
     def search(self):
         BBPastCost.loops = []
         BBPastCost.queue = []
         BBPastCost.queue.append(self)
-        print('push: ' + str(BBPastCost.queue[-1]))
-        print('   d: ' + str(BBPastCost.queue[-1].depth))
-        print('   m: ' + str(BBPastCost.queue[-1].matches))
-        print('   c: ' + str(BBPastCost.queue[-1].cost))
-        print('   q: ' + str(BBPastCost.queue))
+        # print('push: ' + str(BBPastCost.queue[-1]))
+        # print('   d: ' + str(BBPastCost.queue[-1].depth))
+        # print('   m: ' + str(BBPastCost.queue[-1].matches))
+        # print('   c: ' + str(BBPastCost.queue[-1].cost))
+        # print('   q: ' + str(BBPastCost.queue))
         print()
         while len(BBPastCost.queue) > 0:
             next_tree_index = 0
-            for i, t in enumerate(BBPastCost.queue[1:]):
+            for i, t in enumerate(BBPastCost.queue[:]):
                 if BBPastCost.queue[next_tree_index].cost > t.cost:
                     next_tree_index = i
+            print(' chs: ' + str(BBPastCost.queue[next_tree_index]))
+            # print('   d: ' + str(BBPastCost.queue[next_tree_index].depth))
+            # print('   m: ' + str(BBPastCost.queue[next_tree_index].matches))
+            # print('   c: ' + str(BBPastCost.queue[next_tree_index].cost))
             BBPastCost.queue[next_tree_index].process(next_tree_index)
 
     def process(self, q_index):
@@ -55,24 +64,24 @@ class BBPastCost:
             new_state[i] = self.state[i - 2]
             new_state[i - 2] = self.state[i]
             BBPastCost.queue.append(BBPastCost(state=new_state[:], depth=self.depth + 1))
-            print('push: ' + str(BBPastCost.queue[-1]))
-            print('   d: ' + str(BBPastCost.queue[-1].depth))
-            print('   m: ' + str(BBPastCost.queue[-1].matches))
-            print('   c: ' + str(BBPastCost.queue[-1].cost))
-            print('   q: ' + str(BBPastCost.queue))
-            print()
+            # print('push: ' + str(BBPastCost.queue[-1]))
+            # print('   d: ' + str(BBPastCost.queue[-1].depth))
+            # print('   m: ' + str(BBPastCost.queue[-1].matches))
+            # print('   c: ' + str(BBPastCost.queue[-1].cost))
+            # print('   q: ' + str(BBPastCost.queue))
+            # print()
         else:
             new_state = self.state[:]
             i = new_state.index(None)
             new_state[i] = self.state[i + 2]
             new_state[i + 2] = self.state[i]
             BBPastCost.queue.append(BBPastCost(state=new_state[:], depth=self.depth + 1))
-            print('push: ' + str(BBPastCost.queue[-1]))
-            print('   d: ' + str(BBPastCost.queue[-1].depth))
-            print('   m: ' + str(BBPastCost.queue[-1].matches))
-            print('   c: ' + str(BBPastCost.queue[-1].cost))
-            print('   q: ' + str(BBPastCost.queue))
-            print()
+            # print('push: ' + str(BBPastCost.queue[-1]))
+            # print('   d: ' + str(BBPastCost.queue[-1].depth))
+            # print('   m: ' + str(BBPastCost.queue[-1].matches))
+            # print('   c: ' + str(BBPastCost.queue[-1].cost))
+            # print('   q: ' + str(BBPastCost.queue))
+            # print()
         # move right and left
         if self.state.index(None) % 2 == 0:
             new_state = self.state[:]
@@ -80,26 +89,26 @@ class BBPastCost:
             new_state[i] = self.state[i + 1]
             new_state[i + 1] = self.state[i]
             BBPastCost.queue.append(BBPastCost(state=new_state[:], depth=self.depth + 1))
-            print('push: ' + str(BBPastCost.queue[-1]))
-            print('   d: ' + str(BBPastCost.queue[-1].depth))
-            print('   m: ' + str(BBPastCost.queue[-1].matches))
-            print('   c: ' + str(BBPastCost.queue[-1].cost))
-            print('   q: ' + str(BBPastCost.queue))
-            print()
+            # print('push: ' + str(BBPastCost.queue[-1]))
+            # print('   d: ' + str(BBPastCost.queue[-1].depth))
+            # print('   m: ' + str(BBPastCost.queue[-1].matches))
+            # print('   c: ' + str(BBPastCost.queue[-1].cost))
+            # print('   q: ' + str(BBPastCost.queue))
+            # print()
         else:
             new_state = self.state[:]
             i = new_state.index(None)
             new_state[i] = self.state[i - 1]
             new_state[i - 1] = self.state[i]
             BBPastCost.queue.append(BBPastCost(state=new_state[:], depth=self.depth + 1))
-            print('push: ' + str(BBPastCost.queue[-1]))
-            print('   d: ' + str(BBPastCost.queue[-1].depth))
-            print('   m: ' + str(BBPastCost.queue[-1].matches))
-            print('   c: ' + str(BBPastCost.queue[-1].cost))
-            print('   q: ' + str(BBPastCost.queue))
-            print()
+            # print('push: ' + str(BBPastCost.queue[-1]))
+            # print('   d: ' + str(BBPastCost.queue[-1].depth))
+            # print('   m: ' + str(BBPastCost.queue[-1].matches))
+            # print('   c: ' + str(BBPastCost.queue[-1].cost))
+            # print('   q: ' + str(BBPastCost.queue))
+            # print()
         # pop queue
-        print(' pop: ' + str(BBPastCost.queue[q_index]))
+        # print(' pop: ' + str(BBPastCost.queue[q_index]))
         BBPastCost.queue.pop(q_index)
-        print('      ' + str(BBPastCost.queue))
-        print('--------------------------------')
+        # print('      ' + str(BBPastCost.queue))
+        # print('--------------------------------')
