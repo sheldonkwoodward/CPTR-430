d2accb2c7a42a9ed5dfeae0e607b86e398fd904b
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-05-10 09:43:37 -0700

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup
 
 setup(name='sw_cptr_430',
       version='1.0.0',
-      packages=['river_crossing', 'three_puzzle'],
+      packages=['river_crossing', 'three_puzzle', 'send_more_money'],
       entry_points={
           'console_scripts': [
               'river_crossing = river_crossing.__main__:main',
-              'three_puzzle = three_puzzle.__main__:main'
+              'three_puzzle = three_puzzle.__main__:main',
+              'send_more_money = send_more_money.__main__:main'
           ]
       },
       )
